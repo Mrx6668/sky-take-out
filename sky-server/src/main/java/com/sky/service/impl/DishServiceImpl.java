6db37f5f9a8bf21b13d88f060cb9d5eafdfd021c
Mr_X <w2953873192@gamil.com>
2023-08-28 13:21:14 +0800

    插入（菜品信息+动态SQL foreach批量插入口味）

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,44 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Service
+public class DishServiceImpl implements DishService {
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+
+    @Override
+    @Transactional
+    public void saveWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+        //向菜品表插入1条数据
+        dishMapper.insert(dish);
+
+        Long dishId = dish.getId();
+
+
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if (!flavors.isEmpty()){
+            flavors.forEach(dishFlavor -> {
+                dishFlavor.setDishId(dishId);
+            });
+            //向口味表插入n条数
+            //批量插入
+            dishFlavorMapper.insertBatch(flavors);
+
+        }
+    }
+}
