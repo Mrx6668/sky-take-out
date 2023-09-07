011e29d0755e82ebe6aec1ea07e6b6033eb1b33f
Mr_X <w2953873192@gamil.com>
2023-09-07 17:56:54 +0800

    用户端查看购物车功能实现

--- sky-server/src/main/java/com/sky/service/impl/shoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/shoppingCartServiceImpl.java
@@ -64,4 +64,14 @@ public class shoppingCartServiceImpl implements ShoppingCartService {
 
 
     }
+
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        Long currentId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(currentId)
+                .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+    }
 }
